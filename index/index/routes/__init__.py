28f0a0b2ab8c74138ad3aed90213b7cee4139555
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-17 20:43:52 +0200

    remove unsed routers

--- index/index/routes/__init__.py
+++ index/index/routes/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
 from index.routes.index import router as index_router
-from index.routes.publications import router as publications_router
 
 router = APIRouter(prefix="/api")
 router.include_router(index_router, prefix="/index")
-router.include_router(publications_router, prefix="/publications")
