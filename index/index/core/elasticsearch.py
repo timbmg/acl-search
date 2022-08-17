ce55e3e263308b16f95f0425d418ae8ff617691d
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-17 20:42:48 +0200

    file methods

--- index/index/core/elasticsearch.py
+++ index/index/core/elasticsearch.py
@@ -1,10 +1,13 @@
+import logging
 from typing import Dict, List, Union
 
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Index
+from elasticsearch_dsl import Index, Document
 from elasticsearch_dsl.exceptions import ValidationException
-from index.models.elasticsearch.publication import Publication
+from index.models.elasticsearch import File, Publication
 from index.settings import ElasticSearchSettings
+
+logger = logging.getLogger(__name__)
 
 
 class ElasticSearchClient:
@@ -18,22 +21,33 @@ class ElasticSearchClient:
             sniff_on_start=True,
             sniff_on_connection_fail=True,
         )
-        self.create_publications_index_if_not_exists()
+        self.create_index_if_not_exists(File)
+        self.create_index_if_not_exists(Publication)
 
-    def create_publications_index_if_not_exists(self):
-        if not Index("publications").exists(using=self.es):
-            Publication.init(using=self.es)
+    def create_index_if_not_exists(self, model: Document):
+        if not Index(model.Index.name).exists(using=self.es):
+            model.init(using=self.es)
 
     def index_publication(self, publication: Union[Publication, Dict]):
+
+        logger.info("Indexing publication: {}".format(publication["bibkey"]))
+
         if isinstance(publication, dict):
             publication = Publication(**publication, meta={"id": publication["bibkey"]})
         try:
             publication.save(using=self.es)
         except ValidationException as e:
             print(f"Skipping {publication}.\n{e}")
-        self.search_publications.cache_clear()
 
     def index_publications(self, publications: List[Union[Publication, Dict]]):
         # TODO: Bulk indexing
         for publication in publications:
             self.index_publication(publication)
+
+    def index_file(self, file: Union[File, Dict]):
+        if isinstance(file, Dict):
+            file = File(**file, meta={"id": file["name"]})
+        file.save(using=self.es)
+
+    def get_file_by_id(self, _id: str) -> Union[None, File]:
+        return File.get(id=_id, using=self.es, ignore=404)
