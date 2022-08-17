f38619c45c32c3763e5ae7ba355fb9aef92d6fb4
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-17 20:44:04 +0200

    add rabbit and redis settings

--- index/index/settings/__init__.py
+++ index/index/settings/__init__.py
@@ -1,3 +1,5 @@
-from index.settings.app import AppSettings
+from index.settings.rabbitmq import RabbitMQSettings
 from index.settings.elasticsearch import ElasticSearchSettings
 from index.settings.github import GithubSettings
+from index.settings.rabbitmq import RabbitMQSettings
+from index.settings.redis import RedisSettings
