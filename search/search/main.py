d4f195bf25a2272604314325756a67a52df53944
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-21 15:09:08 +0200

    set CORS origin

--- search/search/main.py
+++ search/search/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,7 +10,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=os.getenv("CORS_ALLOW_ORIGINS", "*").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
