17f630d31281115842fb4be0a6cd99f82f42408f
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-22 18:30:13 +0200

    add icons to footer links

--- frontend/src/components/SiteFooter.vue
+++ frontend/src/components/SiteFooter.vue
@@ -11,9 +11,9 @@
       </div>
       <div class="col-4" style="max-width:360px">
         <h5>&#128279;	Links</h5>
-        <div><a href="https://aclanthology.org" target="_blank">ACL Anthology</a></div>
-        <div><a href="https://github.com/timbmg/acl-search" target="_blank">Github</a></div>
-        <div><a href="https://twitter.com/timbmg" target="_blank">Twitter</a></div>
+        <div><a href="https://aclanthology.org" target="_blank"><i class="bi bi-book"></i> ACL Anthology</a></div>
+        <div><a href="https://github.com/timbmg/acl-search" target="_blank"><i class="bi bi-github"></i> Github</a></div>
+        <div><a href="https://twitter.com/timbmg" target="_blank"><i class="bi bi-twitter"></i> Twitter</a></div>
       </div>
     </div>
     </div>
@@ -27,11 +27,7 @@ export default {
 }
 </script>
 <style>
-/* footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-} */
+
 footer {
   margin-top: 20px;
   padding: 20px;
@@ -42,6 +38,10 @@ footer {
 }
 .row {
   text-align: left;
+}
+a {
+
+  color: #2c3e50;
 }
 a:link {
   text-decoration: none;
