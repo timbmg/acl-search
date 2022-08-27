8389e9f37021f26bc3c72aaf715490a97ac85de2
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-08-27 21:33:02 +0200

    remove trailing slash

--- search/search/routes/venues.py
+++ search/search/routes/venues.py
@@ -6,6 +6,6 @@ from search.core import ElasticSearchClient
 router = APIRouter()
 
 
-@router.get("/", response_model=List[Dict])
+@router.get("", response_model=List[Dict])
 def get_venues(elasticsearch_client=Depends(ElasticSearchClient)):
     return elasticsearch_client.get_venues()
